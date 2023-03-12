c4a7f57e962594b89bafaa9e56ddc09608105511
Serg <reznov90210@gmail.com>
2023-03-12 22:42:54 +0300

    Just cosmetic changes in unfree.nix with nixfmt

--- unfree.nix
+++ unfree.nix
@@ -1,14 +1,13 @@
 { lib, pkgs, ... }:
 
 {
-    nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-            "Oracle_VM_VirtualBox_Extension_Pack"
-            "steam"
-            "steam-original"
-            "steam-run"
-            "steam-runtime"
-         ];
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "Oracle_VM_VirtualBox_Extension_Pack"
+      "steam"
+      "steam-original"
+      "steam-run"
+      "steam-runtime"
+    ];
 }
-
-    
 
